d2a5d1df5b46e706d500fd7bfe6c5ddcd2987ba1
Vishnu Satis <satis.vishnu@gmail.com>
2024-09-02 14:47:38 +0530

    Fix for context in RAG builder. (#16)
    Fix for context in RAG builder

--- flo_ai/retrievers/flo_retriever.py
+++ flo_ai/retrievers/flo_retriever.py
@@ -27,7 +27,12 @@ class FloRagBuilder():
                 ("system", """You are an assistant for question-answering tasks. 
                  Use the following pieces of retrieved context to answer the question. 
                  If you don't know the answer, just say that you don't know. 
-                 Use three sentences maximum and keep the answer concise."""),
+                 Use three sentences maximum and keep the answer concise.
+
+                 Here is the context:
+                 {context}
+                 
+                 """),
                 MessagesPlaceholder(variable_name="chat_history"),
                 ("human", "{question}"),
             ]
