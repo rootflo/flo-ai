2376be054037d7525d22e0af21c716f051b95dcf
vizsatiz <satis.vishnu@gmail.com>
2024-09-17 21:26:42 +0530

    LLM router simple implementation

--- flo_ai/router/flo_router_factory.py
+++ flo_ai/router/flo_router_factory.py
@@ -1,5 +1,6 @@
 from flo_ai.state.flo_session import FloSession
 from flo_ai.router.flo_supervisor import FloSupervisor
+from flo_ai.router.flo_llm_router import FloLLMRouter
 from flo_ai.router.flo_linear import FloLinear
 from flo_ai.yaml.flo_team_builder import TeamConfig
 from flo_ai.models.flo_team import FloTeam
@@ -13,5 +14,7 @@ class FloRouterFactory:
             return FloSupervisor.Builder(session, team_config.router.name, flo_team).build()
         elif team_config.router.kind == 'linear':
             return FloLinear.Builder(session, flo_team, team_config.router).build()
+        elif team_config.router.kind == 'llm':
+            return FloLLMRouter.Builder(session, team_config.router.name, flo_team).build()
         else:
             raise Exception("Unknown router type")
