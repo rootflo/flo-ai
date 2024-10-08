4b68e798a2479d1604cda037505fcdcf034babec
vizsatiz <satis.vishnu@gmail.com>
2024-10-08 22:41:43 +0530

    Set version to release 0.0.3-rc1

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="flo-ai",                          # This is the name of the package
-    version="0.0.3",                        # The initial release version
-    author="Vishnu Satis",                  # Full name of the author
+    name="flo-ai",                          
+    version="0.0.3-rc1",                    
+    author="Rootflo",                       
     description="Create composable AI agents",
-    long_description=long_description,      # Long description read from the the readme file
+    long_description=long_description,      
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    packages=setuptools.find_packages(),    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],                                      # Information to filter the project on PyPi website
-    python_requires='>=3.10',               # Minimum version requirement of the package
-    py_modules=["flo"],                     # Name of the python package
-    package_dir={'':'flo'},                 # Directory of the source code of the package
-    install_requires=[]                     # Install other dependencies if any
+    ],                                      
+    python_requires='>=3.10',               
+    py_modules=["flo"],                     
+    package_dir={'':'flo'},                 
+    install_requires=[]
 )
