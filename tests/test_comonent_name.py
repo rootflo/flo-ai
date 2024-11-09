f829dd65d097e0e7fcc844d67315637082881733
vizsatiz <satis.vishnu@gmail.com>
2024-11-09 18:48:20 +0530

    Adding tests using pytest

---
+++ tests/test_comonent_name.py
@@ -0,0 +1,18 @@
+import pytest
+from flo_ai.yaml.validators import raise_for_name_error
+
+@pytest.mark.parametrize('flo_name, validity', [
+    ('CorrectName', True),
+    ('Wrong Name', False),
+    ('correct_name', True),
+    ('correct-name', True),
+    ('wrong/name', False),
+])
+def test_func(flo_name, validity):
+    isException = False
+    try:
+        raise_for_name_error(flo_name)
+    except Exception as e:
+        isException = True
+    assert isException != validity
+    
