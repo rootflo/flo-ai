3490d430857b715f6927dc150f84f282abff8499
Vishnu Satis <satis.vishnu@gmail.com>
2024-12-22 15:08:17 +0530

    release: create 0.0.5-dev4 (#84)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='flo-ai',
-    version='0.0.5-dev3',
+    version='0.0.5-dev4',
     author='Rootflo',
     description='Create composable AI agents',
     long_description=long_description,
