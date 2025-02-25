b5c586f0f270ca7c71fa87c18c3fee0160954cf8
vizsatiz <satis.vishnu@gmail.com>
2025-02-25 13:29:01 +0530

    Setting up 0.0.6-dv1

--- flo_ai/setup.py
+++ flo_ai/setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='flo-ai',
-    version='0.0.5-rc4',
+    version='0.0.6-dev1',
     author='Rootflo',
     description='Create composable AI agents',
     long_description=long_description,
