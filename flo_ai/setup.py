bd3894487978f73eb568c4ad8a90158ce2d5da43
vizsatiz <satis.vishnu@gmail.com>
2025-07-19 16:34:07 +0530

    Fix dependencies and setup rc1 build

--- flo_ai/setup.py
+++ flo_ai/setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='flo-ai',
-    version='0.0.7-rc1',
+    version='1.0.0-rc1',
     author='Rootflo',
     description='Create composable AI agents',
     long_description=long_description,
