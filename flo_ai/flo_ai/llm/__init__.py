fd5605df7b39a7ad40dfbdb4c136655b2e86bc9a
rootflo-hardik <hardik@rootflo.ai>
2025-08-05 13:25:39 +0530

    exporting ImageMessage

--- flo_ai/flo_ai/llm/__init__.py
+++ flo_ai/flo_ai/llm/__init__.py
@@ -1,8 +1,16 @@
-from .base_llm import BaseLLM
+from .base_llm import BaseLLM, ImageMessage
 from .anthropic_llm import Anthropic
 from .openai_llm import OpenAI
 from .ollama_llm import OllamaLLM
 from .gemini_llm import Gemini
 from .openai_vllm import OpenAIVLLM
 
-__all__ = ['BaseLLM', 'Anthropic', 'OpenAI', 'OllamaLLM', 'Gemini', 'OpenAIVLLM']
+__all__ = [
+    'BaseLLM',
+    'Anthropic',
+    'OpenAI',
+    'OllamaLLM',
+    'Gemini',
+    'OpenAIVLLM',
+    'ImageMessage',
+]
