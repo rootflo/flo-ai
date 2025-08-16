3dbaedf049af327082167e2ea6a56aff011c5554
vizsatiz <satis.vishnu@gmail.com>
2025-08-16 13:24:56 +0530

    Add support for vertex ai

---
+++ flo_ai/flo_ai/llm/vertexai_llm.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from google import genai
+from flo_ai.llm.gemini_llm import GeminiLLM
+
+
+class VertexAI(GeminiLLM):
+    def __init__(
+        self,
+        model: str = 'gemini-2.5-flash',
+        temperature: float = 0.7,
+        api_key: Optional[str] = None,
+        base_url: str = None,
+        project: str = None,
+        location: str = None,
+        **kwargs,
+    ):
+        super().__init__(model, api_key, temperature, **kwargs)
+        self.client = (
+            genai.Client(project=project, location=location, vertexai=True)
+            if self.api_key
+            else genai.Client()
+        )
