94d2a7c45fc56c003e807462db931e97bea1a97a
JacobSanosh <jacobsanosh@gmail.com>
2025-10-27 15:46:03 +0530

    fix(flo-ai): formatting the code

--- flo_ai/flo_ai/models/agent.py
+++ flo_ai/flo_ai/models/agent.py
@@ -100,10 +100,14 @@ class Agent(BaseAgent):
                     formatted_doc = await self.llm.format_document_in_message(input)
                     self.add_to_history(MessageType.USER, formatted_doc)
                 elif isinstance(input, ChatMessage):
-                    if isinstance(input.content,ImageMessage):
-                        self.add_to_history(input.role, self.llm.format_image_in_message(input.content))
+                    if isinstance(input.content, ImageMessage):
+                        self.add_to_history(
+                            input.role, self.llm.format_image_in_message(input.content)
+                        )
                     elif isinstance(input.content, DocumentMessage):
-                        formatted_doc = await self.llm.format_document_in_message(input.content)
+                        formatted_doc = await self.llm.format_document_in_message(
+                            input.content
+                        )
                         self.add_to_history(input.role, formatted_doc)
                     else:
                         resolved_content = resolve_variables(input.content, variables)
