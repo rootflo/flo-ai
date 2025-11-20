d97546b556714b81dc4232538afd702781174b6f
vishnu r kumar <rkumar.vishnu28@gmail.com>
2025-11-20 18:48:11 +0530

    fix result iscoroutine check

--- flo_ai/flo_ai/arium/nodes.py
+++ flo_ai/flo_ai/arium/nodes.py
@@ -195,13 +195,12 @@ class FunctionNode:
             )
             return UserMessage(content=result)
 
+        logger.info(f"Executing FunctionNode '{self.name}' as a regular function")
+        result = self.function(inputs=inputs, variables=variables, **kwargs)
+
         if asyncio.iscoroutine(result):
             logger.info(f"Executing FunctionNode '{self.name}' as a coroutine")
             content = await result
             return UserMessage(content=content)
 
-        logger.info(f"Executing FunctionNode '{self.name}' as a regular function")
-        result = self.function(
-            inputs=inputs, variables=variables, **self.prefilled_params, **kwargs
-        )
         return UserMessage(content=result)
