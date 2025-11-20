4b41f9b57df0cae3752ec9b9acda596efc6d8f28
vishnu r kumar <rkumar.vishnu28@gmail.com>
2025-11-20 18:57:15 +0530

    fix ruff formatting

--- flo_ai/flo_ai/arium/nodes.py
+++ flo_ai/flo_ai/arium/nodes.py
@@ -196,7 +196,9 @@ class FunctionNode:
             return UserMessage(content=result)
 
         logger.info(f"Executing FunctionNode '{self.name}' as a regular function")
-        result = self.function(inputs=inputs, variables=variables, **self.prefilled_params, **kwargs)
+        result = self.function(
+            inputs=inputs, variables=variables, **self.prefilled_params, **kwargs
+        )
 
         if asyncio.iscoroutine(result):
             logger.info(f"Executing FunctionNode '{self.name}' as a coroutine")
