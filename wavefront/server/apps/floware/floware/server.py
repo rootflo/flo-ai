d5c2b9afc64d8117cc6aa615dc4198771fd914e3
rootflo-hardik <hardik@rootflo.ai>
2026-09-08 13:20:31 +0530

    removed azure_redis_auth and any ref to it

--- wavefront/server/apps/floware/floware/server.py
+++ wavefront/server/apps/floware/floware/server.py
@@ -27,7 +27,6 @@ from common_module.log.logger import logger
 from common_module.prometheus.prometheus_middleware import PrometheusMiddleware
 from common_module.response_formatter import ResponseFormatter
 
-# from db_repo_module.cache.azure_redis_auth import patch_redis_for_azure
 from db_repo_module.database.connection import DatabaseClient
 from db_repo_module.db_repo_container import DatabaseModuleContainer
 from fastapi import HTTPException
@@ -237,7 +236,6 @@ scheduler_manager = SchedulerManager()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # patch_redis_for_azure()
     # Startup code (runs before the application starts)
     logger.info('Starting application...')
 
